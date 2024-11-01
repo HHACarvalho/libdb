6b69cd69c313b4cb99771c9ff8b0a40a6bb28356
Hugo Carvalho <hhac@protonmail.com>
2024-11-01 10:57:23 +0000

    Adds .env to hide the sensitive data from the shell script

--- publish.sh
+++ publish.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Create a docker network to enable all the microservices to communicate
+# Load the environment variables
+source .env
+
+# Create a docker network to enable all the microservices to communicate between themselves
 docker network create libdb-network
 
 # Stop and remove any existing SQL Server container
 docker stop libdb-sqlserver && docker rm libdb-sqlserver
 
 # Create and run the SQL Server container
-docker run -d -p 1401:1433 --network libdb-network --name libdb-sqlserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Docker@1!" mcr.microsoft.com/mssql/server:2022-latest
+docker run -d -p 1401:1433 --network libdb-network --name libdb-sqlserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SA_PASSWORD" mcr.microsoft.com/mssql/server:2022-latest
 
 # Publish the .NET back-end application
 cd libdb-dotnet/
