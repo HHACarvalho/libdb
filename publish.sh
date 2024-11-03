8a12389759f037f91b50fcbc9f25e9dc45a5389b
Hugo Carvalho <hhac@protonmail.com>
2024-11-03 15:55:18 +0000

    Updates to the publishing shell script

--- publish.sh
+++ publish.sh
@@ -3,19 +3,8 @@
 # Load the environment variables
 source .env
 
-# Create a docker network to enable all the microservices to communicate between themselves
-docker network create libdb-network
+# Stop and remove any containers
+docker-compose down
 
-# Stop and remove any existing SQL Server container
-docker stop libdb-sqlserver && docker rm libdb-sqlserver
-
-# Create and run the SQL Server container
-docker run -d -p 1401:1433 --network libdb-network --name libdb-sqlserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SA_PASSWORD" mcr.microsoft.com/mssql/server:2022-latest
-
-# Publish the .NET back-end application
-cd libdb-dotnet/
-./publish.sh
-
-# Publish the Svelte front-end application
-cd ../libdb-svelte/
-./publish.sh
+# Build all images and run every container
+docker-compose up --build -d
